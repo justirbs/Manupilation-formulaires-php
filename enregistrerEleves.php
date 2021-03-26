0316fd6f5d76ab895c0e37af86046628174a091d
Justine <ribasjusti@eisti.eu>
2021-03-26 22:06:57 +0100

    exo 1 terminé

--- enregistrerEleves.php
+++ enregistrerEleves.php
@@ -5,11 +5,11 @@
 </head>
 <body>
   <?php
-    $fp = fopen('infoEleves.csv', 'a+');
-    foreach ($_POST as $valeur) {
-      fputcsv($fp, $valeur,";");
-    }
+    $fp = fopen('./infoEleves.csv', 'w');
+    fputcsv($fp, $_POST,";");
     fclose($fp);
   ?>
+  <h2>L'élève a bien été enregistré</h2>
+  <a href="inscriptionEleves.php">Revenir à la page précédente</a>
 </body>
 </html>
